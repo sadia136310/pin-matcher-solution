afc310280a2f22b4fb623eb4befee916499c2078
sadia136310 <sadia1363@gmail.com>
2022-08-21 12:57:10 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,81 @@
+function getPin() {
+    const pin = generatePin();
+    const pinString = pin + '';
+    if (pinString.length === 4) {
+        return pin;
+    }
+    else {
+        return getPin();
+
+    }
+}
+
+
+
+function generatePin() {
+    const random = Math.round(Math.random() * 10000);
+    return random;
+
+
+}
+
+document.getElementById('generate-pin').addEventListener('click', function () {
+
+    const pin = getPin();
+
+    // display pin 
+    const displayPinField = document.getElementById('display-pin');
+
+    displayPinField.value = pin;
+});
+
+
+document.getElementById('calculator').addEventListener('click', function (event) {
+
+    const number = event.target.innerText;
+    const typedNumberField = document.getElementById('typed-numbers');
+    const previousTypedNumber = typedNumberField.value;
+    if (isNaN(number)) {
+        if (number === 'C') {
+            typedNumberField.value = '';
+        }
+
+        else if (number === '<') {
+            const digits = previousTypedNumber.split('');
+            digits.pop();
+            const remainingDigits = digits.join('');
+            typedNumberField.value = remainingDigits;
+        }
+    }
+    else {
+
+        const newTypedNumber = previousTypedNumber + number;
+        typedNumberField.value = newTypedNumber;
+    }
+
+})
+
+document.getElementById('submit-pin').addEventListener('click', function () {
+    const displayPinField = document.getElementById('display-pin');
+    const currentPin = displayPinField.value;
+
+    const typedNumberField = document.getElementById('typed-numbers');
+    const typedNumber = typedNumberField.value;
+
+    const pinSuccess = document.getElementById('pin-success');
+    const pinFailure = document.getElementById('pin-failure');
+    if (typedNumber === currentPin) {
+
+        pinSuccess.style.display = 'block';
+        pinFailure.style.display = 'none';
+    }
+    else {
+
+        pinFailure.style.display = 'block';
+        pinSuccess.style.display = 'none';
+
+    }
+})
+
+
+
